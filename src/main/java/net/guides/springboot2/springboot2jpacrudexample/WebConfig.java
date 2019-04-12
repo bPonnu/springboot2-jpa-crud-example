3986dd25aa4205137bf622324537172dc411125f
Biraveenmaks Ponnu <biraveenmaks.ponnu@zuehlke.com>
2019-04-12 15:34:14 +0200

    added a comment in WebConfig.java

--- src/main/java/net/guides/springboot2/springboot2jpacrudexample/WebConfig.java
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//for more infos: https://www.baeldung.com/spring-cors
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
