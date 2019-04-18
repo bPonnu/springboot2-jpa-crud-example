e47536e3bca5e7c71c293cf5aa56579b2cdd1ba1
Biraveenmaks Ponnu <biraveenmaks.ponnu@zuehlke.com>
2019-04-18 15:30:44 +0200

    updated the admin password

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO employees (first_name, last_name, email_address, user_Name, password, user_Role) VALUES
-  ('admin', 'admin', 'admin', 'admin', 'admin', 'ADMINISTRATOR');
+  ('admin', 'admin', 'admin', 'admin', '$2a$10$uZZANImGtVBAGuxZZLY17OPYfdR7Dhk4.mUi.G9U./cdHLBVZzUq2', 'ADMINISTRATOR');
